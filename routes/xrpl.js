44f3606a83522a4cfd83c349e36942bcb11a7003
Santhosh <santhosh.s@avyata.com>
2021-10-28 14:19:05 +0530

    Database sync feature added

--- routes/xrpl.js
+++ routes/xrpl.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
+const moment = require("moment");
 const CsvParser = require("json2csv").Parser;
 const { createPDFBase64, createPDFBinary } = require("../libs/PDFController");
 const BalanceSheetPDFTemplate = require("../libs/PDFController/Templates/BalanceSheetTemplate");
-
-router.get("/", async function (req, res, next) {
-  let data = await getBalance("rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn");
-  res.send(data);
-});
 
 router.post("/get-balance", async function (req, res, next) {
   try {
@@ -123,9 +119,115 @@ router.post("/get-balance/pdf", async function (req, res, next) {
   }
 });
 
+router.post("/db/connection", async function (req, res, next) {
+  try {
+    let { host, port, user, password } = req.body;
+    let mysql = require("mysql2/promise");
+    let connection = await mysql.createConnection({
+      host,
+      port,
+      user,
+      password,
+    });
+    const [rows, fields] = await connection.execute("SELECT 1 + 1 AS result");
+    if (rows[0].result === 2) {
+      return res
+        .status(200)
+        .send({ status: true, message: "Connection successful!" });
+    }
+    return res
+      .status(400)
+      .send({ status: false, message: "Authentication failed." });
+  } catch (err) {
+    if (err.details) {
+      return res
+        .status(400)
+        .send({ status: false, message: err.details[0].message });
+    } else {
+      console.log(err);
+      return res.status(500).send({
+        status: false,
+        message: err.message ? err.message : "Internal Server Error.",
+      });
+    }
+  }
+});
+
+router.post("/db/sync", async function (req, res, next) {
+  let accounts_data;
+  try {
+    if (
+      req.body.accounts &&
+      req.body.accounts instanceof Array &&
+      req.body.accounts.length
+    ) {
+      let requests = await req.body.accounts.map((account) =>
+        getBalance(account)
+      );
+      accounts_data = await Promise.all(requests);
+    } else {
+      throw {
+        details: [
+          { message: "Error! Please provide atleast one account adderess." },
+        ],
+      };
+    }
+
+    let { host, port, user, password } = req.body;
+    let mysql = require("mysql2/promise");
+    let connection = await mysql.createConnection({
+      host,
+      port,
+      user,
+      password,
+    });
+    const [rows, fields] = await connection.execute("SELECT 1 + 1 AS result");
+    if (rows[0].result === 2) {
+      await connection.execute(
+        `CREATE DATABASE IF NOT EXISTS ${req.body.database}`
+      );
+      await connection.execute(
+        `DROP TABLE IF EXISTS ${req.body.database}.${moment().format(
+          "YYYYMMDDHH"
+        )}`
+      );
+      await connection.execute(
+        `CREATE TABLE ${req.body.database}.${moment().format(
+          "YYYYMMDDHH"
+        )} (id int NOT NULL AUTO_INCREMENT, address varchar(255), balance varchar(255), ledger_index varchar(255), flags varchar(255), PRIMARY KEY (id))`
+      );
+      let insert_query = `INSERT INTO ${req.body.database}.${moment().format(
+        "YYYYMMDDHH"
+      )} (address, balance, ledger_index, flags) values`;
+      accounts_data.forEach((obj, index) => {
+        insert_query += `('${obj.result.account_data.Account}', '${obj.result.account_data.Balance}', '${obj.result.ledger_current_index}', '${obj.result.account_data.Flags}')`;
+        if (index !== accounts_data.length - 1) {
+          insert_query += ",";
+        }
+      });
+      await connection.execute(insert_query);
+      return res.status(200).send({ status: true });
+    }
+    return res
+      .status(400)
+      .send({ status: false, message: "Authentication failed." });
+  } catch (err) {
+    if (err.details) {
+      return res
+        .status(400)
+        .send({ status: false, message: err.details[0].message });
+    } else {
+      console.log(err);
+      return res.status(500).send({
+        status: false,
+        message: err.message ? err.message : "Internal Server Error.",
+      });
+    }
+  }
+});
+
 router.get("/transactions/:account", async function (req, res, next) {
   try {
-    console.log(req.params);
     const payload = {
       method: "account_tx",
       params: [
